202a38ab3e3168cf0542b92b5387cb083ed5982b
Sunny <jacksunderlin@gmail.com>
2022-09-17 14:38:16 -0600

    Establish 16x16 grid

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 // Initialize 16x16 grid of divs
 const container = document.querySelector(".gridcontainer");
-console.log(container);
+for (let i = 0; i < 256; i++) {
+    cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.appendChild(cell);
+}
