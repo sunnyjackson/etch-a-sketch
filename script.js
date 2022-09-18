73a88fe4559e37de83f7b92052ab4ed8d821afa5
Sunny <jacksunderlin@gmail.com>
2022-09-18 13:26:58 -0600

    Implement Rainbow Mode and Eraser Mode

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+const Modes = {
+    SingleColor: 0,
+    Eraser: 1,
+    Rainbow: 2
+}
+
 DEFAULT_GRID_SIZE = 16;
+DEFAULT_PAINT_COLOR = 'black';
+DEFAULT_MODE = Modes.SingleColor;
+
 isPainting = false;
+paintColor = DEFAULT_PAINT_COLOR;
+paintMode = DEFAULT_MODE;
 
 function generateGrid(size) {
     // Remove existing children cells
@@ -20,22 +31,47 @@ function generateGrid(size) {
 }
 
 function applyColor() {
-    if (isPainting) {
-        this.classList.add("painted");
+    if (!isPainting) return;
+
+    switch(paintMode) {
+        case Modes.SingleColor:
+            this.style.backgroundColor = paintColor;
+            return;
+        case Modes.Eraser:
+            this.style.backgroundColor = "white";
+            return;
+        case Modes.Rainbow:
+            const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+            const r = randomBetween(0, 255);
+            const g = randomBetween(0, 255);
+            const b = randomBetween(0, 255);
+            this.style.backgroundColor = `rgb(${r},${g},${b})`; // Collect all to a css color string
+            return;
     }
 }
 
 function clearGrid() {
     for(let cell of gridcontainer.childNodes) {
-        cell.removeAttribute('class');
-        cell.classList.add('griditem');
+        cell.style.backgroundColor = "white";
     }
+}
+
+function transitionBlackPenMode() {
+    paintMode = Modes.SingleColor;
+    paintColor = "black";
+}
+
+function transitionRainbowPenMode() {
+    paintMode = Modes.Rainbow;
+}
+
+function transitionEraserMode() {
+    paintMode = Modes.Eraser;
 }
 
 // Establish grid
 document.body.onmouseup = () => isPainting = false;
 document.body.addEventListener("mousedown", () => isPainting = true, {capture: true}); // capture this event, so that isPainting is set to True before trying to paint the first cell
-
 const gridcontainer = document.querySelector(".gridcontainer");
 generateGrid(DEFAULT_GRID_SIZE);
 
@@ -49,6 +85,10 @@ slider.oninput = function() {
     slider_value.innerHTML = gridsize;
 }
 
-// Establish clear button
+// Establish buttons
+document.getElementById("black-button").onclick = transitionBlackPenMode;
+document.getElementById("rainbow-button").onclick = transitionRainbowPenMode;
+document.getElementById("eraser-button").onclick = transitionEraserMode;
+
 let clear_button = document.getElementById("clear-button");
 clear_button.onclick = clearGrid;
