151f9975d70a0163bd5d6cf565abf6fd5f3ec256
Sunny <jacksunderlin@gmail.com>
2022-09-18 09:26:53 -0600

    Add hover effect

--- script.js
+++ script.js
@@ -2,11 +2,16 @@ function generateGrid(size) {
     gridcontainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     gridcontainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     for (let i = 0; i < size*size; i++) {
-        cell = document.createElement("div");
+        let cell = document.createElement("div");
         cell.classList.add("griditem");
         gridcontainer.appendChild(cell);
     }
 }
 
+// Establish grid
 const gridcontainer = document.querySelector(".gridcontainer");
 generateGrid(16);
+for (let cell of document.getElementsByClassName("griditem")){
+    cell.onmouseover = () => cell.classList.add("hover");
+    cell.onmouseout = () => cell.classList.remove("hover");
+}
